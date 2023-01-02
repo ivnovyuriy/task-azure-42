abbdf058b0d51c60b47819bb69c354c4c1e62574
ivnovyuriy <ivanovyuriy@gmail.com>
2023-01-02 14:23:15 +0300

    Make all the staff working

---
+++ README.md
@@ -0,0 +1,8 @@
+1. You plan to move backup files and documents from an on-premises Windows file server to Azure Storage. The backup files will be stored as blobs. You need to create a storage account named CorpStorage01. The solution must meet the following requirements: 
+
+  •  Ensure that the documents are accessible via drive mappings from Azure virtual machines that run Windows Server 2016 
+  •  Provide the highest possible redundancy for the documents 
+  •  Minimize storage access costs
+
+2. ** You need to automate tasks utilising IaaC tools of your choice (For e.g. Terraform).
+
---
+++ storage.tf
@@ -0,0 +1,43 @@
+# Generate random string to get unique Storage account name
+resource "random_string" "random" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+# Create Azure Storage account
+resource "azurerm_storage_account" "CorpStorage01" {
+  name                     = "itrastorage${random_string.random.id}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  # Enable cool access tier to reduce storage access costs
+  access_tier              = "Cool"
+  # Enable maximum available redundancy level for this account
+  account_replication_type = "GRS"
+}
+
+# Create a share for drive mapping in Windows VM
+resource "azurerm_storage_share" "BackupShare" {
+  name                 = "backup"
+  quota                = 5
+  storage_account_name = azurerm_storage_account.CorpStorage01.name
+  depends_on           = [azurerm_storage_account.CorpStorage01]
+}
+
+# Create a blob container
+resource "azurerm_storage_container" "BlobContainer" {
+  name                  = "blobcontainer"
+  storage_account_name  = azurerm_storage_account.CorpStorage01.name
+  container_access_type = "private"
+}
+
+# Add network security rules for storage account
+resource "azurerm_storage_account_network_rules" "storage_net_rules" {
+  resource_group_name        = azurerm_resource_group.rg.name
+  storage_account_name       = azurerm_storage_account.CorpStorage01.name
+  default_action             = "Deny"
+  bypass                     = ["AzureServices"]
+  ip_rules                   = ["85.107.4.0/24"]
+  virtual_network_subnet_ids = [azurerm_subnet.storage_subnet.id]
+}
